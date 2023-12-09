6e4944889e2cfc5c30042262b16aae35d9cb545b
anmanasyan <annamanasyan2002@gmail.com>
2023-12-09 20:13:10 +0400

    adding config for dim. table

---
+++ pipeline_dimensional_data/config.py
@@ -0,0 +1,5 @@
+# Configuration for the pipeline
+
+input_dir = 'C:\\Users\\Anna\\Desktop\\BI\\Group_Project_2\\pipeline_dimensional_data\\queries'
+
+sql_server_config = 'C:\\Users\\Anna\\Desktop\\BI\\Group_Project_2\\sql_server_config.cfg'
