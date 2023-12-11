6885c0c4de19f9b51f952f4189c1f9b50917b79a
anmanasyan <annamanasyan2002@gmail.com>
2023-12-12 00:39:30 +0400

    adding log_loc to config

--- pipeline_relational_data/config.py
+++ pipeline_relational_data/config.py
@@ -3,3 +3,5 @@
 input_dir = 'C:\\Users\\Anna\\Desktop\\BI\\Group_Project_2\\pipeline_relational_data\\queries'
 
 sql_server_config = 'C:\\Users\\Anna\\Desktop\\BI\\Group_Project_2\\sql_server_config.cfg'
+
+log_loc = 'C:\\Users\\Anna\\Desktop\\BI\\Group_Project_2\\logs\\logs_relational_data_pipeline.txt'
